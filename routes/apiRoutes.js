08438facc36750e7605d7802f8297fabaf07ea0f
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-03 07:17:59 +0000

    Get and Post functionality working. Need to fix delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,74 +1,46 @@
 const fs = require("fs");
-const savedNotes = require("../db/db.json");
+const notes = require("../db/db.json")
 
 
 module.exports = function(app) {
 
-    function writeToDB(notes){
-        // Converts new JSON Array back to string
-        notes = JSON.stringify(notes);
-        console.log (notes);
-        // Writes String back to db.json
-        fs.writeFileSync("./db/db.json", notes, function(err){
-            if (err) {
-                return console.log(err);
-            }
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    });
+
+    app.get("/api/notes/:id", function(req, res) {
+        res.json(notes[Number(req.params.id)]);
+    });
+
+    app.post("/api/notes", function(req, res) {
+
+        let newNote = req.body;
+        let uniqueId = (notes.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        notes.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(notes);    
+
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        console.log(`Deleting note with id ${noteId}`);
+        notes = notes.filter(currentNote => {
+           return currentNote.id != noteId;
         });
-    }
-
-    //========== API ROUTES ==========
-
-    // GET Method to return all notes
-    app.get("/api/notes", function(req, res){
-        res.json(savedNotes);
-    });
-
-    // POST Method to add notes
-    app.post("/api/notes", function(req, res){
-
-        // Set unique id to entry
-        if (savedNotes.length == 0){
-            req.body.id = "0";
-        } else{
-            req.body.id = JSON.stringify(JSON.parse(savedNotes[savedNotes.length - 1].id) + 1);
+        for (currentNote of notes) {
+            currentNote.id = newId.toString();
+            newId++;
         }
-        
-        console.log("req.body.id: " + req.body.id);
-
-        // Pushes Body to JSON Array
-        savedNotes.push(req.body);
-
-        // Write notes data to database
-        writeToDB(savedNotes);
-        console.log(savedNotes);
-
-        // returns new note in JSON format.
-        res.json(req.body);
-    });
-
-    // DELETE Method to delete note with specified ID
-    app.delete("/api/notes/:id", function(req, res){
-        
-        // Obtains id and converts to a string
-        let id = req.params.id.toString();
-        console.log(id);
-
-        // Goes through notesArray searching for matching ID
-        for (i=0; i < savedNotes.length; i++){
-           
-            if (savedNotes[i].id == id){
-                console.log("match!");
-                // responds with deleted note
-                res.send(savedNotes[i]);
-
-                // Removes the deleted note
-                savedNotes.splice(i,1);
-                break;
-            }
-        }
-
-        // Write notes data to database
-        writeToDB(savedNotes);
-
-    });
-};
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+        res.json(notes);
+    }); 
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
-var path = require("path");
+const path = require("path");
 
-module.exports = function(app){
-
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"))
-  })
-
-  app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-  })
+module.exports = function(app) {
+    
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
 }

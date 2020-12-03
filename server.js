3c6538a061a625102eb734ba0c496645bc34130e
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-03 06:03:52 +0000

    Setting up server

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static('public'));
+
+
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});

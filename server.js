27fbf91781da46d7b655179d41a3b77da900737f
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-04 20:02:18 +0000

    change to package.json for heroku upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 // Creating server application on port 3000
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }))
